993d65f18e2b964fe58c4ebb416c996f131fcf0c
twój login do GitHuba <jekaterina.zielinska@gmail.com>
2023-03-10 16:20:05 +0100

    creating new user

--- util/auth.js
+++ util/auth.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_KEY = 'AIzaSyCBOOU19tedM_f6xOykfJDfgbQVUU0ZLYA';
 
-async function createUser(email, password) {
+export async function createUser(email, password) {
   const response = await axios.post(
     'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
     {
@@ -13,5 +13,3 @@ async function createUser(email, password) {
   );
 
 }
-
-export default createUser;
